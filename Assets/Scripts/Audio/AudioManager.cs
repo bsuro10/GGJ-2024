76837e18cfd52ee5eb99ee54ed4e52ddda91f840
fkeyz <fkeyzuwu@gmail.com>
2024-01-25 23:33:19 +0200

    initialize 3d project

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    public Sound[] sounds;
+
+    private Dictionary<string, Sound> soundsDict = new Dictionary<string, Sound>();
+
+    public AudioMixerGroup masterGroup;
+    public AudioMixerGroup musicGroup;
+    public AudioMixerGroup sfxGroup;
+
+    [Header("Music Sources")]
+    [SerializeField] private AudioSource forestMusic;
+    [SerializeField] private AudioSource endingMusic;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+        foreach(Sound sound in sounds)
+        {
+            AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false;
+            float volume = sound.volume;
+            audioSource.volume = volume;
+            audioSource.outputAudioMixerGroup = sfxGroup;
+            sound.source = audioSource;
+            soundsDict[sound.name] = sound;
+        }
+    }
+
+    public void PlaySound(string name)
+    {
+        Sound sound = soundsDict[name];
+        sound.source.PlayOneShot(sound.GetClip());
+    }
+
+    public void PlayFinalMusic()
+    {
+        forestMusic.Stop();
+        endingMusic.Play();
+    }
+
+    public void PlayMainMusic()
+    {
+        forestMusic.Play();
+        endingMusic.Stop();
+    }
+
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+        [HideInInspector] public AudioSource source;
+        public AudioClip[] clips;
+        private int currentClipIndex = -1;
+        [Range(0.0001f, 1f)] public float volume;
+
+        public AudioClip GetClip()
+        {
+            if(currentClipIndex + 1 < clips.Length)
+            {
+                currentClipIndex++;
+            }
+            else
+            {
+                currentClipIndex = 0;
+            }
+
+            return clips[currentClipIndex];
+        }
+    }
+}
+
---
+++ Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static PauseMenu Instance;
+    private bool isGamePaused = false;
+    private bool isInOptionsMenu = false;
+    [SerializeField] private GameObject pauseMenuUi;
+    [SerializeField] private GameObject optionsMenuUi;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Toggle()
+    {
+        if (isInOptionsMenu)
+        {
+            CloseOptionsMenu();
+            return;
+        }
+
+        isGamePaused = !isGamePaused;
+        pauseMenuUi.SetActive(isGamePaused);
+        Time.timeScale = isGamePaused ? 0f : 1f;
+    }
+
+    public void ResumeGame()
+    {
+        isGamePaused = false;
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void RestartLevel()
+    {
+        Toggle();
+        /*GameObject.Find("LevelManager").GetComponent<LevelManager>().RestartLevel();*/
+    }
+
+    public void OpenOptionsMenu()
+    {
+        pauseMenuUi.SetActive(false);
+        optionsMenuUi.SetActive(true);
+        isInOptionsMenu = true;
+    }
+
+    public void CloseOptionsMenu()
+    {
+        pauseMenuUi.SetActive(true);
+        optionsMenuUi.SetActive(false);
+        isInOptionsMenu = false;
+    }
+
+    public void ChangeMasterVolume(float volume)
+    {
+        AudioManager.Instance.masterGroup.audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void ChangeMusicVolume(float volume)
+    {
+        AudioManager.Instance.masterGroup.audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void ChangeSfxVolume(float volume)
+    {
+        AudioManager.Instance.masterGroup.audioMixer.SetFloat("SfxVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
