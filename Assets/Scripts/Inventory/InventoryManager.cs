da7bf672d527ef41cad6e56b94af38a33a523e2f
Ran Biderman <bsuro10@gmail.com>
2024-01-26 01:32:11 +0200

    Add Inventory, Collectible and Interactable scripts

---
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    #region Singleton
+    public static InventoryManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    #endregion
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    [SerializeField] private int slots = 20;
+    [SerializeField] public List<InventoryItem> items;
+
+    private void Start()
+    {
+        items = new List<InventoryItem>();
+    }
+
+    public void Add(Item item)
+    {
+        Add(item, 1);
+    }
+
+    public bool Add(Item item, int amount)
+    {
+        if (!item.isDefaultItem)
+        {
+            if (items.Count >= slots)
+            {
+                Debug.Log("Not enough room in inventory");
+                return false;
+            }
+
+            int inventoryItemIndex = items.FindIndex(inventoryItem => inventoryItem.item.Equals(item));
+            if (inventoryItemIndex != -1)
+                items[inventoryItemIndex].amount += amount;
+            else
+                items.Add(new InventoryItem(item, amount));
+
+            if (onItemChangedCallback != null)
+                onItemChangedCallback.Invoke();
+
+        }
+        return true;
+    }
+
+    public void Remove(Item item)
+    {
+        Remove(item, 1);
+    }
+
+    public void Remove(Item item, int amount)
+    {
+        int inventoryItemIndex = items.FindIndex(inventoryItem => inventoryItem.item.Equals(item));
+        if (inventoryItemIndex != -1)
+        {
+            items[inventoryItemIndex].amount -= amount;
+            if (items[inventoryItemIndex].amount <= 0)
+                items.RemoveAt(inventoryItemIndex);
+        }
+
+        if (onItemChangedCallback != null)
+            onItemChangedCallback.Invoke();
+    }
+
+}
