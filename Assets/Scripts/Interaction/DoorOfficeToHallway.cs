e38a818471a6843c40b0b5396d7a4eebecb35fc3
fkeyz <fkeyzuwu@gmail.com>
2024-01-28 00:00:41 +0200

    fixwalking bug

--- Assets/Scripts/Interaction/DoorOfficeToHallway.cs
+++ Assets/Scripts/Interaction/DoorOfficeToHallway.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class DoorOfficeToHallway : Interactable
 {
     public override void Interact()
     {
         GetComponentInParent<Door>().BashOpenDoor();
+        var footswitcher = FindFirstObjectByType<FirstPersonController>().GetComponent<FootstepSoundSwitcher>();
+        footswitcher.StopForGood();
     }
 
     public override string GetText()
