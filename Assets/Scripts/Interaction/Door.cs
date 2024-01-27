b564a5e1eb8ebb254e1847c86e89ba67cd3df3f6
fkeyz <fkeyzuwu@gmail.com>
2024-01-27 02:56:17 +0200

    not working

--- Assets/Scripts/Interaction/Door.cs
+++ Assets/Scripts/Interaction/Door.cs
@@ -5,11 +5,37 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private GameObject door;
+    [SerializeField] private GameObject doorForcePosition;
+
     [ContextMenu("Open")]
     public void Open()
     {
         animator.SetBool("isOpen", true);
         AudioManager.Instance.PlaySound("open_door");
+    }
+
+
+    [ContextMenu("Open slightly")]
+    public void OpenSlightly()
+    {
+        animator.SetTrigger("openSlightly");
+        AudioManager.Instance.PlaySound("open_door_slightly");
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C)) OpenSlightly();
+        else if (Input.GetKeyDown(KeyCode.V)) BashOpenDoor();
+    }
+    [ContextMenu("Bash open door")]
+    public void BashOpenDoor()
+    {
+        Rigidbody doorBody = door.GetComponent<Rigidbody>();
+        doorBody.isKinematic = false;
+        doorBody.AddForce((transform.forward + transform.up * 0.5f).normalized * 50f, ForceMode.Impulse);
+        doorBody.AddTorque(new Vector3(10f, 5f, 0f) * 500f, ForceMode.Impulse);
+        //AudioManager.Instance.PlaySound("bash_open_door");
     }
 
     [ContextMenu("Close")]
