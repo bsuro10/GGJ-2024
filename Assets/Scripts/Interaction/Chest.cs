2658089044c67b86aa6ec0a498b3ff913e7a31f7
fkeyz <fkeyzuwu@gmail.com>
2024-01-28 00:14:37 +0200

    easy puzzle sound

--- Assets/Scripts/Interaction/Chest.cs
+++ Assets/Scripts/Interaction/Chest.cs
@@ -7,11 +7,16 @@ public class Chest : Interactable
 {
     [SerializeField] private Animator animator;
     [SerializeField] private UnityEvent callback;
+    [SerializeField] private bool firstChest;
 
     public override void Interact()
     {
         gameObject.layer = LayerMask.NameToLayer("Default");
         animator.SetBool("isOpen", true);
+        if (firstChest)
+        {
+            AudioManager.Instance.PlayVoiceline("easy_puzzle");
+        }
     }
 
     public override string GetText()
